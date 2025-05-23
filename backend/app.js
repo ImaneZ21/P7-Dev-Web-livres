57a308373dd69a9c3c538dd9e26d09995beff4d4
ImaneZ21 <zerdag.imane@gmail.com>
2025-05-23 13:26:20 +0200

    Add route post / get

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,7 +11,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/books", (req, res, next) => {
+app.get("/api/books", (req, res, next) => {
     console.log('Route /api/books appelée');
   const book = [
     {
@@ -268,6 +270,20 @@ app.use("/api/books", (req, res, next) => {
 
   res.status(200).json(book)
 });
+
+app.post('/api/auth/login', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'objet créé'
+  })
+})
+
+app.post('/api/auth/signup', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'user créé'
+  })
+})
 
 module.exports = app;
 
