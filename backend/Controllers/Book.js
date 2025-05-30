28331ae69bd281451638201c4ced56d409cbd42a
ImaneZ21 <zerdag.imane@gmail.com>
2025-05-30 13:43:23 +0200

    Create Book Controllers

---
+++ backend/Controllers/Book.js
@@ -0,0 +1,45 @@
+const Book = require('../Models/Book');
+
+exports.createBook =  (req, res) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body,
+  });
+  book
+    .save()
+    .then(() => res.status(201).json({ message: "Book créé !" }))
+    .catch((error) => res.status(400).json({ error }));
+}
+
+exports.getBooks =  (req, res) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+}
+
+exports.getBestRatings =  (req, res) => {
+  Book.find()
+    .sort({ averageRating: -1})
+    .limit(3)
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(400).json({ error }));
+}
+
+exports.modifyBook = (req, res) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Book modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+}
+
+exports.deleteBook = (req, res) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Book supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+}
+
+exports.getOneBook =  (req, res) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(400).json({ error }));
+}
+
--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
+const bookRoutes = require("./Routes.js/Book.js")
+
 mongoose
   .connect(
     "mongodb+srv://imane:9HhFgPmEs2Nvgtte@cluster0.rdcqu9q.mongodb.net/vieuxgrimoire?retryWrites=true&w=majority"
@@ -23,60 +26,8 @@ app.use((req, res, next) => {
   next();
 });
 
-const Book = require("./Models/Book");
-const User = require("./Models/User");
+app.use('/api', bookRoutes);
 
-app.post("/api/books", (req, res) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body,
-  });
-  book
-    .save()
-    .then(() => res.status(201).json({ message: "Book enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("/api/books", (req, res) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("/api/books/:id", (req, res) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.put("/api/books/:id", (req, res) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Book modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.delete('/api/books/:id', (req, res) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Book supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.post("/api/auth/login", (req, res) => {
-  const user = new User({
-    ...req.body,
-  });
-  user
-    .save()
-    .then(() => res.status(200).json({ message: "User authentifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.post("/api/auth/signup", (req, res) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "user créé",
-  });
-});
 
 module.exports = app;
 
